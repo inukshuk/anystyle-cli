73bbcfa9cf92d5812bdf26e3a30a29bcbe2a8c6b
Zac <165101547+ColourBlindHobbiest@users.noreply.github.com>
2025-05-10 12:27:08 +1000

    Update base.rb to include ris command

--- lib/anystyle/cli/commands/base.rb
+++ lib/anystyle/cli/commands/base.rb
@@ -49,6 +49,8 @@ module AnyStyle
             AnyStyle.parser.format_bibtex(dataset).to_s
           when 'csl'
             JSON.pretty_generate AnyStyle.parser.format_csl(dataset)
+          when 'ris'
+            JSON.pretty_generate AnyStyle.parser.format_ris(dataset)
           when 'json'
             JSON.pretty_generate AnyStyle.parser.format_hash(dataset)
           when 'ref', 'txt'
